adbe8c49b548e9f18899458cf21cdb8809990786
Story DeWeese <sdeweese@scu.edu>
2019-09-16 19:40:13 -0700

    Fix CAPS phone number

--- src/screens/Help.js
+++ src/screens/Help.js
@@ -58,7 +58,7 @@ class Help extends React.Component {
             <TouchableOpacity onPress={() => this.props.navigation.navigate('Caps')}>
               <Text style= {styles.text1}>CAPS: Counseling and Psychological Services</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={()=>{Linking.openURL('tel:40855444501');}}>
+            <TouchableOpacity onPress={()=>{Linking.openURL('tel:4085544501');}}>
               <Text style= {styles.text2}>To make an appointment for same or next day (M-F, 9am-3pm), call 408-554-44501.</Text>
             </TouchableOpacity>
           </View>
