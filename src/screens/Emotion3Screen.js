308dd68a6a13e6b0a0ba0e545a8e0ddb9b050591
Story DeWeese <sdeweese@scu.edu>
2019-09-23 17:12:18 -0700

    Revert "Go from Emotion3 to Journal"
    This reverts commit 8d973541bab8910a42d84b493a72daa705b68ba1.

--- src/screens/Emotion3Screen.js
+++ src/screens/Emotion3Screen.js
@@ -69,7 +69,7 @@ class Emotion3Screen extends React.Component {
       console.log("There was an error saving the product");
     });
 
-    this.props.navigation.navigate('Diary');
+    this.props.navigation.navigate('Dashboard');
   }
 
   renderElement() {
