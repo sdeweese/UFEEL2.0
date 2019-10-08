c366d04c8343738c42643c7a12494eaf529d9efd
Story DeWeese <sdeweese@scu.edu>
2019-10-07 18:15:45 -0700

    Add Google Docs link to Safety Plan

--- src/screens/SafetyPlanScreen.js
+++ src/screens/SafetyPlanScreen.js
@@ -24,15 +24,20 @@ class SafetyPlan extends React.Component {
             <TouchableOpacity onPress={()=>{Linking.openURL('tel:8002738255');}}>
               <Text style= {styles.text2}>To chat with a Crisis Counselor 24/7 about you or someone you are concerned about, call 1-800-273-8255.</Text>
             </TouchableOpacity>
-          </View>
-          <View style= {styles.cont}>
+        </View>
+        <View style= {styles.cont}>
             <TouchableOpacity onPress={() => Linking.openURL('https://www.thetrevorproject.org/')}>              
               <Text style= {styles.text1}>Trevor Project</Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={()=>{Linking.openURL('tel:8664887386');}}>
               <Text style= {styles.text2}>If you’re a member of the LGBTQ+ community thinking about suicide, you deserve immediate help - please call the Trevor Lifeline at 866-488-7386.</Text>
             </TouchableOpacity>
-          </View>
+        </View>
+        <View style= {styles.cont}>
+            <TouchableOpacity onPress={() => Linking.openURL('https://docs.google.com/document/d/1ati707c38BVhA55Hk62hjayg0qqRAl0kqZh-LgERGY4/edit?usp=sharing')}> 
+              <Text style= {styles.text1}>Safety Plan</Text>
+            </TouchableOpacity>
+        </View>
       </LinearGradient>
     );
   }
